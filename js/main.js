e3aca38550fa9cbe06b6edd611f76cb37d5a6c63
Samandar0021305 <TSamandardev89@gmail.com>
2023-01-17 12:45:06 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ let elBox = document.querySelector(".box")
 let isX = true
 
 elBox.addEventListener("click",(e)=>{
+    let elid = e.target.id
+    elid = elid.slice(1)
+    elid = parseInt(elid,10)
+    console.log(elid);
     if(isX){
         if(e.target.textContent === ""){
             e.target.textContent = "X"
             isX = !isX
-        }
+         }
     }else{
         if(e.target.textContent === ""){
             e.target.textContent = '0'
