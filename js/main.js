77254a86aaddbe7cf6f0f48cfe7e14311a696374
Samandar0021305 <TSamandardev89@gmail.com>
2023-01-17 12:29:45 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 let elBox = document.querySelector(".box")
 
+let isX = true
+
 elBox.addEventListener("click",(e)=>{
-    e.target.textContent = "X"
+    if(isX){
+        e.target.textContent = "X"
+    }else{
+        e.target.textContent = '0'
+    }
+
+    isX = !isX
 })
